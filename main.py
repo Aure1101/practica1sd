7603813406b7c1cafd843b27b18a3cef72321e7f
HPCornejo <4f20perezcornejohector@gmail.com>
2024-09-10 10:43:48 -0600

    Se añadio clientes

--- main.py
+++ main.py
@@ -16,6 +16,7 @@ MONGO_URI = 'mongodb://localhost:27017'
 client = motor_asyncio.AsyncIOMotorClient(MONGO_URI)
 db = client['sistemas_distribuidos']
 users_collection = db['users']
+clientes_collection = db['Clientes']
 
 
 # Objeto para interactuar con la API
@@ -41,3 +42,49 @@ class Clientes(BaseModel):
     nombre: str
     apellido: str
     correo_electronico: str
+
+@app.get('/clientes/')
+async def get_clientes():
+    cl = dict()
+    clientes = await clientes_collection.find().to_list(None)
+
+    for i, cliente in enumerate(clientes):
+        cl[i] = dict()
+        cl[i]["nombre"] = cliente["nombre"]
+        cl[i]["apellido"] = cliente["apellido"]
+        cl[i]["correo_electronico"] = cliente["correo_electronico"]
+    return cl
+
+@app.get('/clientes/')
+async def get_cliente_id():
+    clientes = await clientes_collection.find().to_list(None)
+    for cliente in clientes:
+        clientes=['_id'] = str(cliente['_id'])
+
+@app.post('/clientes/')
+async def create_cliente(cliente: Clientes):
+    await clientes_collection.insert_one(cliente.dict())
+    return{
+        'message': "Cliente added succesfully"
+    }
+
+@app.put('/clientes/{cli_id}')
+async def update_cliente(cli_id, cli: Clientes):
+    clientes = await clientes_collection.find().to_list(None)
+    for _cli in clientes:
+        if str(_cli['_id']) == cli_id:
+            await clientes_collection.update_one(_cli, {'$set': cli.dict()})
+            return{
+                'message': f'Cliente {cli_id} updated successfully'
+            }
+
+@app.delete('/clientes/{cli_id}')
+async def delete_cliente(cli_id, cli: Clientes):
+    clientes = await clientes_collection.find().to_list(None)
+    for _cli in clientes:
+        if str(_cli['_id']) == cli_id:
+            await clientes_collection.delete_one(_cli, {'$set': cli.dict()})
+            return {
+                'message': 'The client has been eliminated'
+            }
+        raise HTTPException(status_code=404, detail=f'Client: {cli_id} not found')
