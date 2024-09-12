b80b5c82e9e276eb09f1777636efe66f5c2fd820
Aure1101 <111398414+Aure1101@users.noreply.github.com>
2024-09-11 23:02:33 -0600

    hot fixes in main.py

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ cat_collection = db['Categorias']
 users_collection = db['users']
 clientes_collection = db['Clientes']
 productos_collection = db['Productos']
-
+pedidos_collection = db['Pedidos']
 
 # Objeto para interactuar con la API
 app = FastAPI()
@@ -61,7 +61,7 @@ async def get_clientes():
 async def get_cliente_id():
     clientes = await clientes_collection.find().to_list(None)
     for cliente in clientes:
-        clientes=['_id'] = str(cliente['_id'])
+        clientes['_id'] = str(cliente['_id'])
 
 @app.post('/clientes/')
 async def create_cliente(cliente: Clientes):
@@ -187,3 +187,58 @@ async def delate_productos(pro_id, pro:Productos):
        }
 
     raise HTTPException(status_code=404, detail=f'Producto {pro_id} not found')
+
+
+@app.get('/pedidos/')
+async def get_pedidos():
+    pedidos = await pedidos_collection.find().to_list(length=1)
+    for pedido in pedidos:
+        pedido['_id'] = str(pedido['_id'])
+    return pedidos
+
+@app.get('/user/{id}')
+async def get_categoria(id: str):
+    if pedidos_collection.find_one({"_id": ObjectId(id)}) is not None:
+        pedido = await pedidos_collection.find_one({"_id": ObjectId(id)})
+        pedido['_id'] = str(pedido['_id'])
+        return pedido
+
+    return HTTPException(status_code=404, detail=f"Error")
+
+@app.post('/pedidos/')
+async def create_user(pedido: Pedidos):
+    await pedidos_collection.insert_one(pedido.model_dump())
+    return {
+        'message': 'El usuario se creó'
+    }
+
+@app.delete("/delete/pedido/{id}")
+async def delete_student(id: str):
+
+    delete_result = await pedidos_collection.delete_one({"_id": ObjectId(id)})
+
+    if delete_result.deleted_count == 1:
+        return Response("Se eliminó")
+
+    raise HTTPException(status_code=404, detail=f"Error")
+
+@app.put("/update/pedido/{id}")
+async def update_pedido(id: str, pedido: Pedidos):
+    pedido = {
+        k: v for k, v in pedido.model_dump(by_alias=True).items() if v is not None
+    }
+    pedido['fecha'] = str(pedido['fecha'])
+
+    if len(pedido) >= 1:
+        update_result = await pedidos_collection.find_one_and_update(
+            {"_id": ObjectId(id)},
+            {"$set": pedido},
+        )
+    
+    if update_result is not None:
+        update_result['_id'] = str(update_result['_id'])
+        return update_result
+    
+   
+    raise HTTPException(status_code=404, detail=f"Error")
+    
